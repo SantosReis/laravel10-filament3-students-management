20e212cea43ec807b39d567f5d2dcf5b2dde1eae
Marcus Reis <mvsantosreis@gmail.com>
2024-10-26 20:07:51 +0100

    Display Classes and Section Data

--- app/Filament/Resources/SectionResource.php
+++ app/Filament/Resources/SectionResource.php
@@ -4,28 +4,37 @@ namespace App\Filament\Resources;
 
 use Filament\Forms;
 use Filament\Tables;
-use App\Models\Classes;
+use App\Models\Section;
+use Filament\Forms\Get;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Validation\Rules\Unique;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
-use App\Filament\Resources\ClassesResource\Pages;
+use App\Filament\Resources\SectionResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use App\Filament\Resources\ClassesResource\RelationManagers;
+use App\Filament\Resources\SectionResource\RelationManagers;
 
-class ClassesResource extends Resource
+class SectionResource extends Resource
 {
-    protected static ?string $model = Classes::class;
+    protected static ?string $model = Section::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                TextInput::make('name'),
-            ]);
+        ->schema([
+            Select::make('class_id')
+                ->relationship(name: 'class', titleAttribute: 'name'),
+            TextInput::make('name')
+                ->unique(ignoreRecord: true, modifyRuleUsing: function (Get $get, Unique $rule) {
+                    return $rule->where('class_id', $get('class_id'));
+            }),
+        ]);
     }
 
     public static function table(Table $table): Table
@@ -33,7 +42,7 @@ class ClassesResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('name'),
-                TextColumn::make('sections.name')
+                TextColumn::make('class.name')
                     ->badge(),
             ])
             ->filters([
@@ -60,9 +69,9 @@ class ClassesResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListClasses::route('/'),
-            'create' => Pages\CreateClasses::route('/create'),
-            'edit' => Pages\EditClasses::route('/{record}/edit'),
+            'index' => Pages\ListSections::route('/'),
+            'create' => Pages\CreateSection::route('/create'),
+            'edit' => Pages\EditSection::route('/{record}/edit'),
         ];
     }
 }
